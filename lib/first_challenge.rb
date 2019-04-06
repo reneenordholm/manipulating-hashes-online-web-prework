f4e5ec35771164069cdb9662c6b37df5fb907ea4
Renee Nordholm <reneenordholm@gmail.com>
2019-04-06 22:10:23 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
   contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if("strawberry")
-
+end
   #remember to return your newly altered contacts hash!
   contacts
 end
