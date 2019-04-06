dac8704a6df8f238cda78715ccb32a1eb9bb4abb
Renee Nordholm <reneenordholm@gmail.com>
2019-04-06 22:08:57 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-
+  contacts["Jon Snow"][:favorite_icecream_flavors].delete_if("strawberry")
 
   #remember to return your newly altered contacts hash!
   contacts
